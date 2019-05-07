a57792b8890f52290262fcfb0eadf2776e0dd9f9
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-07 19:38:05 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,16 +1,4 @@
 <?php
+$s=$_POST['last'];
 
-$host="localhost";
-$user="root";
-$password="";
-
-$connect=mysqli_connect($host,$user,$password,"figer");
-
-mysqli_set_charset($connect,"UTF8");
-
-if($connect)
-{
-echo '1';
-}else{
-	echo'Error';
-}
+echo $s;
