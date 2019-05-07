79c5adc30bc9436130c57ba67a83a7edc8fd62ad
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-08 03:22:42 +0700

    Update test.php

--- test.php
+++ test.php
@@ -5,15 +5,32 @@ $access_token = 'n3Ip66xMPuO1xND8801hh9NZhuyHgsSuFvCETfyga18qvVuO095cmHbr9mV+M4k
 $strAccessToken = "n3Ip66xMPuO1xND8801hh9NZhuyHgsSuFvCETfyga18qvVuO095cmHbr9mV+M4kejFHkGb88rpwscKSr0co8BpWr8zN09hfRNUvhH6Mp/NOp6dMl/ULggahkDbLHk2nq/CtV0+85qZGZinIv50f6sQdB04t89/1O/w1cDnyilFU=";
  
 $strUrl = "https://api.line.me/v2/bot/message/push";
+
+$host="db4free.net";
+$user="poomzatan123456";
+$password="0811582889zX";
+$connect=mysqli_connect($host,$user,$password,"testdb1234567");
+mysqli_set_charset($connect,"UTF8");
+if($connect)
+{
+        $sqltext1 = "SELECT * FROM Line ORDER BY `pk_l` DESC LIMIT 1";
+		$qury1 = mysqli_query($connect,$sqltext1);
+		$result=mysqli_fetch_array($qury1,MYSQLI_ASSOC);
+		echo $result['iduserLine'];
  
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
-$arrPostData['to'] = "U9fab035a6b8d51bd7c3104855fd54fc9";
+$arrPostData['to'] = $result['iduserLine'];
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "test";
+}
+else{
+}
+
+
  
  
 $ch = curl_init();
