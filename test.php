2b3d9ada3faa258eaf505972dc52b0edd90ed042
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-07 16:31:11 +0700

    Update test.php

--- test.php
+++ test.php
@@ -7,5 +7,4 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+echo '2';
