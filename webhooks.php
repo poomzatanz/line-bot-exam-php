733cb8b101677e91952f060d4fb5f1cd195eb21f
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-08 03:08:41 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -2,14 +2,11 @@
 $host="db4free.net";
 $user="poomzatan123456";
 $password="0811582889zX";
-
 $connect=mysqli_connect($host,$user,$password,"testdb1234567");
-
 mysqli_set_charset($connect,"UTF8");
-
 if($connect)
 {
-   echo '$r'; 
-	}
-}else{
+   echo 'gg'; 
+}
+else{
 }
