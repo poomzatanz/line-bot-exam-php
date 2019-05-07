47152d6b894370d81d23ab6e8e21d330bb8c32d2
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-07 16:45:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -36,7 +36,15 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
  
+ $arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
+$arrPostData = array();
+$arrPostData['to'] = "USER_ID";
+$arrPostData['messages'][0]['type'] = "U9fab035a6b8d51bd7c3104855fd54fc";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
