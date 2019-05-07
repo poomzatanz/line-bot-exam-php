fb1409650c04b636126650e9fe14d8650cd9ac49
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-07 14:07:22 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,7 +1,7 @@
 <?php // callback.php
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = '69b26e9c5bf7e6a14127a832e313f73e';
+$access_token = '/sF18/CfwHJCVh7fTPWcyAnMOSr72AVmkh42A97KcvSAi/qHMupZf98A9CWTHsiqjFHkGb88rpwscKSr0co8BpWr8zN09hfRNUvhH6Mp/NOoKLiLJBQAE+tERgg37w1ZB5V/4OICwVvgtpPQrEmZ3wdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
