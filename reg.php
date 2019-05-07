5c2c395c9e72313425c1a8d43656dd7c6bc5d718
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-07 18:06:04 +0700

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,12 +1,42 @@
 <?php
-$host="db4free.net";
-$user="poomzatan123456";
-$password="0811582889zX";
-$connect=mysqli_connect($host,$user,$password,"testdb1234567");
-mysqli_set_charset($connect,"UTF8");
-if($connect)
-{
-echo '1';
-}else{
-	echo'Error';
-}
+<html><head><title>เพิ่มข้อมูลหนังสือ</title></head>
+<body >
+<form enctype="multipart/form-data" name="save" method="post"
+action="savebook.php">
+<BR><BR>
+<table width="650" border="1" bgcolor="#FFFFFF" align = "center">
+<tr>
+<td colspan="6" bgcolor = "#3399CC" align = "center" height="21">
+<b>: : เพิ่มข้อมูลหนังสือ : </td>
+</tr>
+<tr>
+<td width = "200">ชื่อ : </td><td width = "400">
+<input type="text" name="BookID" size="10" maxlength="5"> </td>
+</tr>
+<tr >
+<td width = "200" >นามสกุล :</td><td >
+<input type="text" name="BookName" size="50" maxlength="50"> </td>
+</tr>
+</tr>
+<tr >
+<td width = "200" >Email :</td><td >
+<input type="text" name="BookName" size="50" maxlength="50"> </td>
+</tr>
+</tr>
+<tr >
+<td width = "200" >ที่อยู่ :</td><td >
+<textarea id="adress" name="adress" placeholder="Your address..." style="height:200px"></textarea> </td>
+</tr>
+</tr>
+<tr >
+<td width = "200" >เบอร์โทร :</td><td >
+<input type="text" name="BookName" size="50" maxlength="50"> </td>
+</tr>
+</table>
+<BR>
+<div align = "center">
+<input type="submit" name="Submit" value="บันทึกข้อมูล" style="cursor:hand">
+<input type="reset" name="Reset" value="ยกเลิก" style="cursor:hand">
+</div>
+</body>
+</html>
