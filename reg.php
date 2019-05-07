d91e4b670666f43c6f0250d2de19ca10b46bef1a
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-07 17:59:49 +0700

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,12 @@
+<?php
+$host="db4free.net";
+$user="poomzatan123456";
+$password="0811582889zX";
+$connect=mysqli_connect($host,$user,$password,"testdb1234567");
+mysqli_set_charset($connect,"UTF8");
+if($connect)
+{
+echo '1';
+}else{
+	echo'Error';
+}
