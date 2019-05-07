c045eb8eca9b31e9b640cba78f23852cd0c4c459
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-07 16:33:39 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,10 @@
 <?php
 require "vendor/autoload.php";
-
+$access_token = 'n3Ip66xMPuO1xND8801hh9NZhuyHgsSuFvCETfyga18qvVuO095cmHbr9mV+M4kejFHkGb88rpwscKSr0co8BpWr8zN09hfRNUvhH6Mp/NOp6dMl/ULggahkDbLHk2nq/CtV0+85qZGZinIv50f6sQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '69b26e9c5bf7e6a14127a832e313f73e';
+$idPush = 'U9fab035a6b8d51bd7c3104855fd54fc9'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
 echo '2';
