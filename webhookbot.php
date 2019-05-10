a5ad017157ad8c78077463c162bc3f1a58c0ac46
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-10 14:47:16 +0700

    Update webhookbot.php

--- webhookbot.php
+++ webhookbot.php
@@ -10,9 +10,6 @@
    //รับ id ของผู้ใช้
    $id = $arrayJson['events'][0]['source']['userId'];
    if($message == "name"){
-      $push[0]='สวัสดีครับ  ';
-      $push[1]='User ID ของคุณคือ '.;
-      $push[2]=$id;
        for($i=0;$i<=2;$i++){
           $arrayPostData['to'] = $id;
           $arrayPostData['messages'][0]['type'] = "text";
