5a71fc43b95c736d99231e11766956fc4765938a
poomzatanz <50354939+poomzatanz@users.noreply.github.com>
2019-05-10 14:11:29 +0700

    Update webhookbot.php

--- webhookbot.php
+++ webhookbot.php
@@ -1,60 +1,23 @@
-<?php 
-
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$strAccessToken = "QbPV0K1fLygsgn1qPdqb93NdTIqcMUOr4G4ArHZKbwqVqGRSrRzJrmVD9OuIBVzUoo1Zckc2sfsXkwgnxn92+0ZkaCCHq/KHD7QANBAogMPDp5ID+ea2juiV8+VAa8Pjsul37/1/RQlhV7z1ES5oYAdB04t89/1O/w1cDnyilFU=";
- 
-$content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
- 
-$strUrl = "https://api.line.me/v2/bot/message/reply";
-
- $host="db4free.net";
-$user="poomzatan123456";
-$password="0811582889zX";
-$connect=mysqli_connect($host,$user,$password,"testdb1234567");
-mysqli_set_charset($connect,"UTF8");
-if($connect){
-	
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-if($arrJson['events'][0]['message']['text'] == "name"){
- 
-  $push[0]='สวัสดีครับ คุณ ';
-  $push[1]='User ID ของคุณคือ';
-  $push[2]=$arrJson['events'][0]['source']['userId'];
-	
-  for($i=0;$i<=2;$i++){	
-	  $arrayPostData['to'] = $arrJson['events'][0]['source']['userId'];
+<?php
+   $accessToken = "";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+   $content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+   $arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   //รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+   //รับ id ของผู้ใช้
+   $id = $arrayJson['events'][0]['source']['userId'];
+   if($message == "name"){
+       for($i=1;$i<=10;$i++){
+          $arrayPostData['to'] = $id;
           $arrayPostData['messages'][0]['type'] = "text";
-          $arrayPostData['messages'][0]['text'] = $push[$i];
-          pushMsg($arrayHeader,$arrayPostData);  
-  }
-  
-}else{
- $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "sss";
-}
-
- 
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
-}else{
-	
-}
- function pushMsg($arrayHeader,$arrayPostData){
+          $arrayPostData['messages'][0]['text'] = $i;
+          pushMsg($arrayHeader,$arrayPostData);
+       }
+    }
+   function pushMsg($arrayHeader,$arrayPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL,$strUrl);
@@ -67,3 +30,5 @@ curl_close ($ch);
       $result = curl_exec($ch);
       curl_close ($ch);
    }
+   exit;
+
