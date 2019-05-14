6e4acec749620050fb47fc7618d7801e17307ced
poomzatan <poomzatan@gmail.com>
2019-05-14 17:40:59 +0700

    หก

--- test.php
+++ test.php
@@ -12,6 +12,7 @@ $connect=mysqli_connect($host,$user,$password,"testdb1234567");
 mysqli_set_charset($connect,"UTF8");
 if($connect)
 {
+	
 	$n=$_POST['name'];
 	$l=$_POST['last'];
 	$p=$_POST['pass'];
@@ -23,24 +24,28 @@ if($connect)
 		$qury1 = mysqli_query($connect,$sqltext1);
 		$result=mysqli_fetch_array($qury1,MYSQLI_ASSOC);
 		
- 
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-$arrPostData = array();
-$arrPostData['to'] = $result['idLine'];
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "คุณได้สมัครสมาชิกแล้ว	ชื่อของคุณคือ 	".$n." อีเมลล์คือ	".$m."	ที่อยู่	".$a."	เบอร์โทรที่ติดต่อได้	".$t;
-$sqltext = "INSERT INTO `reg` (`pk_r`, `name`, `add`, `email`, `password`, `tel`, `line`) VALUES (NULL, '$n', '$a', '$m', '$pass', '$t','".$result['id']."')";
-  echo $sqltext;
-	$qury = mysqli_query($connect,$sqltext);
-	if($qury){
+		if(isset($result['id'])){
+			$arrHeader = array();
+			$arrHeader[] = "Content-Type: application/json";
+			$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+
+			$arrPostData = array();
+			$arrPostData['to'] = $result['idLine'];
+			$arrPostData['messages'][0]['type'] = "text";
+			$arrPostData['messages'][0]['text'] = "คุณได้สมัครสมาชิกแล้ว	ชื่อของคุณคือ 	".$n." อีเมลล์คือ	".$m."	ที่อยู่	".$a."	เบอร์โทรที่ติดต่อได้	".$t;
+			$sqltext = "INSERT INTO `reg` (`pk_r`, `name`, `add`, `email`, `password`, `tel`, `line`) VALUES (NULL, '$n', '$a', '$m', '$pass', '$t','".$result['id']."')";
+  		echo $sqltext;
+				$qury = mysqli_query($connect,$sqltext);
+				if($qury){
                echo"            <script>
 				window.location='reg.php';
 				</script>
 				";
 	}	
+		}else{
+			echo "<h1>กรุณาสมัครที่ Line ครับ</h1>"
+		}
+
 }
 else{
 }
