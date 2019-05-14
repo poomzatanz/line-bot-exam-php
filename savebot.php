82a5db128a822e21387e16096f44e88ccd0d85eb
poomzatan <poomzatan@gmail.com>
2019-05-14 14:59:32 +0700

    ss

--- savebot.php
+++ savebot.php
@@ -6,13 +6,24 @@ $connect=mysqli_connect($host,$user,$password,"testdb1234567");
 mysqli_set_charset($connect,"UTF8");
 if($connect)
 {
-	$n=$_POST['name'];
-	$l=$_POST['last'];
-    $sqltext = "INSERT INTO `Learn` (`id_learn`, `input`, `out`) VALUES (NULL, '$n', '$l');";
+    $n=$_POST['name'];
+    $l=$_POST['last'];
+    
+    $sqltext1 = "SELECT * FROM `Learn` WHERE input = '".$n."'";
+		$qury1 = mysqli_query($connect,$sqltext1);
+        $result=mysqli_fetch_array($qury1,MYSQLI_ASSOC);
+        echo $sqltext1;
+    if(!$result){
+        $sqltext = "INSERT INTO `Learn` (`id_learn`, `input`, `out`) VALUES (NULL, '$n', '$l');";
     $qury = mysqli_query($connect,$sqltext);
+    
 	if($qury){
                
 		echo "<h1>ขอบคุณสำหรับการสอนนะครับ...</h1>";
-	}	
+	}
+    }else {
+        echo "<h1>มีข้อมูลแล้วครับ.......</h1>";
+    }
+    	
 
 }
