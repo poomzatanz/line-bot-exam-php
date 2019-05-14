777c98a37d0e30a0380c08da28bdcee8523d256c
poomzatan <poomzatan@gmail.com>
2019-05-14 15:23:44 +0700

    ss

--- savebot.php
+++ savebot.php
@@ -1,4 +1,9 @@
 <?php
+$accessToken = "U0p/EAti+Q5I+NI7Mj1X4jyXcF0I33C3aHAATpQpcjq7CpD751Sa35u+iB/k8C59oo1Zckc2sfsXkwgnxn92+0ZkaCCHq/KHD7QANBAogMOzXFbHufeLFmAy8FMz2Hd1DhmC2JxhRFtT5aFui7huWQdB04t89/1O/w1cDnyilFU=";
+$content = file_get_contents('php://input');
+$arrayJson = json_decode($content, true);
+
+
 $host="db4free.net";
 $user="poomzatan123456";
 $password="0811582889zX";
@@ -18,12 +23,39 @@ if($connect)
     $qury = mysqli_query($connect,$sqltext);
     
 	if($qury){
-               
-		echo "<h1>ขอบคุณสำหรับการสอนนะครับ...</h1>";
+        $sqltext1 = "SELECT * FROM `idLine` ORDER BY `id` DESC LIMIT 1";
+		$qury1 = mysqli_query($connect,$sqltext1);
+        $result1=mysqli_fetch_array($qury1,MYSQLI_ASSOC);
+
+        $arrHeader = array();
+        $arrHeader[] = "Content-Type: application/json";
+        $arrHeader[] = "Authorization: Bearer {$accessToken}";
+         
+        $arrPostData = array();
+        $arrPostData['to'] = $result1['idLine'];
+        $arrPostData['messages'][0]['type'] = "text";
+        $arrPostData['messages'][0]['text'] = "ขอบคุณสำหรับการสอนนะครับ";
+
+        echo "<h1>ขอบคุณสำหรับการสอนนะครับ...</h1>";
+        exit;
 	}
     }else {
         echo "<h1>มีข้อมูลแล้วครับ.......</h1>";
+        exit;
     }
     	
 
 }
+function pushMsg($arrayHeader,$arrayPostData){
+    $strUrl = "https://api.line.me/v2/bot/message/push";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+ }
