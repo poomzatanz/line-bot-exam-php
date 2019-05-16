16a051b5db5b1fc4c6ee25ede8aef6a5dcc248dc
poomzatan <poomzatan@gmail.com>
2019-05-16 11:53:06 +0700

    f

--- reg.php
+++ reg.php
@@ -34,9 +34,12 @@ action="test.php">
 <td width = "200" >เบอร์โทร :</td><td >
 <input type="text" name="tel" size="50" maxlength="50"> </td>
 </tr>
+<tr>
+<td><input type="submit" name="Submit" value="บันทึกข้อมูล" style="cursor:hand"></td>
+<td><input type="reset" name="Reset" value="ยกเลิก" style="cursor:hand"></td>
+</tr>
 </table>
-<input type="submit" name="Submit" value="บันทึกข้อมูล" style="cursor:hand">
-<input type="reset" name="Reset" value="ยกเลิก" style="cursor:hand">
+
 
 <script>
   window.fbAsyncInit = function() {
